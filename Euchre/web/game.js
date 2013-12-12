72af65af7dbfe8e666342e73476f796963b0266b
Ethan J. Johnson <ethanjohnson89@gmail.com>
2013-12-11 22:29:46 -0500

    incremental commit

--- Euchre/web/game.js
+++ Euchre/web/game.js
@@ -13,6 +13,7 @@ var pickCardNode;
 
 var readyButtonSpan;
 var suitSelectButtonsSpan;
+var pickUpButtonsSpan;
 
 var bottomNameInd;
 var topNameInd;
@@ -36,12 +37,17 @@ var leftRightTrumpYes;
 var topBottomTrumpInd;
 var leftRightTrumpInd;
 
+var whoseTurnInd;
+var infoNode;
+
 // DEBUG
 var globalResponseXML;
 
 // We need to keep track of these so we know when we've started a new hand (and can tell who won the last one),
 // by comparing them with the latest ones sent by the server.
-
+var whoseTurn; // 1, 2, 3, or 4
+var dealer;
+var respondToCardClick; // boolean
 
 function init()
 {
@@ -84,6 +90,7 @@ function init()
     
     readyButtonSpan = document.getElementById("42");
     suitSelectButtonsSpan = document.getElementById("41");
+    pickUpButtonsSpan = document.getElementById("pickUpButtons");
     
     bottomNameInd = document.getElementById("bottom_name");
     topNameInd = document.getElementById("top_name");
@@ -105,9 +112,13 @@ function init()
     topBottomTrumpYes = document.getElementById("team1TrumpYes");
     leftRightTrumpYes = document.getElementById("team2TrumpYes");
     topBottomTrumpInd = document.getElementById("team1TrumpInd");
-    leftRightTrumpYes = document.getElementById("team2TrumpInd");
+    leftRightTrumpInd = document.getElementById("team2TrumpInd");
+    
+    whoseTurnInd = document.getElementById("whoseTurn");
+    infoNode = document.getElementById("info");
     
     // init value globals
+    respondToCardClick = false;
     
     doAjaxRequest("", "");
 }
@@ -146,7 +157,7 @@ function updateGameState(connection)
         
         var teamNodes = responseXMLRoot.getElementsByTagName("team");
         var yourTeamNum;
-        if(teamNodes[0].getElementsByTagName("you"))
+        if(teamNodes[0].getElementsByTagName("you")[0])
             yourTeamNum = 1;
         else
             yourTeamNum = 2;
@@ -154,7 +165,7 @@ function updateGameState(connection)
         var playerNodes = responseXMLRoot.getElementsByTagName("player");
         var yourPlayerIndex, leftPlayerIndex, topPlayerIndex, rightPlayerIndex;
         var yourPlayerNumber, leftPlayerNumber, topPlayerNumber, rightPlayerNumber;
-        if(playerNodes[0].getElementsByTagName("you"))
+        if(playerNodes[0].getElementsByTagName("you")[0])
         {
             yourPlayerNumber = 1;
             yourPlayerIndex = 0;
@@ -165,7 +176,7 @@ function updateGameState(connection)
             rightPlayerNumber = 4;
             rightPlayerIndex = 3;
         }
-        else if(playerNodes[1].getElementsByTagName("you"))
+        else if(playerNodes[1].getElementsByTagName("you")[0])
         {
             yourPlayerNumber = 3;
             yourPlayerIndex = 1;
@@ -176,7 +187,7 @@ function updateGameState(connection)
             rightPlayerNumber = 2;
             rightPlayerIndex = 2;
         }
-        else if(playerNodes[2].getElementsByTagName("you"))
+        else if(playerNodes[2].getElementsByTagName("you")[0])
         {
             yourPlayerNumber = 2;
             yourPlayerIndex = 2;
@@ -187,7 +198,7 @@ function updateGameState(connection)
             rightPlayerNumber = 1;
             rightPlayerIndex = 0;
         }
-        else if(playerNodes[3].getElementsByTagName("you"))
+        else if(playerNodes[3].getElementsByTagName("you")[0])
         {
             yourPlayerNumber = 4;
             yourPlayerIndex = 3;
@@ -215,5 +226,139 @@ function updateGameState(connection)
         leftNameInd.childNodes[0].data = leftPlayerName;
         topNameInd.childNodes[0].data = topPlayerName;
         rightNameInd.childNodes[0].data = rightPlayerName;
+        
+        var phase = Number(responseXMLRoot.getElementsByTagName("phase")[0].childNodes[0].data);
+        
+        if(playerNodes[0].getElementsByTagName("hasTurn")[0])
+            whoseTurn = 1;
+        else if(playerNodes[1].getElementsByTagName("hasTurn"[0]))
+            whoseTurn = 3;
+        else if(playerNodes[2].getElementsByTagName("hasTurn")[0])
+            whoseTurn = 2;
+        else if(playerNodes[3].getElementsByTagName("hasTurn")[0])
+            whoseTurn = 4;
+        whoseTurnInd.childNodes[0].data = "Player " + whoseTurn;
+        
+        if(playerNodes[0].getElementsByTagName("dealer")[0])
+            dealer = 1;
+        else if(playerNodes[1].getElementsByTagName("dealer")[0])
+            dealer = 3;
+        else if(playerNodes[2].getElementsByTagName("dealer")[0])
+            dealer = 2;
+        else if(playerNodes[3].getElementsByTagName("dealer")[0])
+            dealer = 4;
+        
+        bottomDealerInd.style.display = "none";
+        leftDealerInd.style.display = "none";
+        topDealerInd.style.display = "none";
+        rightDealerInd.style.display = "none";
+        
+        console.log("dealer is" + dealer);
+            console.log("you are player " + yourPlayerNumber);
+            console.log("turn is" + whoseTurn);
+        
+        if(yourPlayerNumber === dealer)
+            bottomDealerInd.style.display = "block";
+        else if(leftPlayerNumber === dealer)
+            leftDealerInd.style.display = "block";
+        else if(topPlayerNumber === dealer)
+            topDealerInd.style.display = "block";
+        else if(rightPlayerNumber === dealer)
+            rightDealerInd.style.display = "block";
+        
+        readyButtonSpan.style.display = "none";
+        suitSelectButtonsSpan.style.display = "none";
+        pickUpButtonsSpan.style.display = "none";
+        respondToCardClick = false;
+        
+        // Update scores and # of tricks taken
+        var team1Score = Number(teamNodes[0].getElementsByTagName("score")[0].childNodes[0].data);
+        var team2Score = Number(teamNodes[1].getElementsByTagName("score")[0].childNodes[0].data);
+        var team1Tricks = Number(teamNodes[0].getElementsByTagName("tricks")[0].childNodes[0].data);
+        var team2Tricks = Number(teamNodes[1].getElementsByTagName("tricks")[0].childNodes[0].data);
+        var topBottomScore, leftRightScore, topBottomTricks, leftRightTricks;
+        if(yourTeamNum === 1)
+        {
+            topBottomScore = team1Score;
+            leftRightScore = team2Score;
+            topBottomTricks = team1Tricks;
+            leftRightTricks = team2Tricks;
+            topBottomTeamInd.childNodes[0].data = "Team 1";
+            leftRightTeamInd.childNodes[0].data = "Team 2";
+        }
+        else
+        {
+            topBottomScore = team2Score;
+            leftRightScore = team1Score;
+            topBottomTricks = team2Tricks;
+            leftRighttricks = team1Tricks;
+            topBottomTeamInd.childNodes[0].data = "Team 2";
+            leftRightTeamInd.childNodes[0].data = "Team 1";
+        }
+        topBottomScoreInd.childNodes[0].data = "Score: " + topBottomScore;
+        leftRightScoreInd.childNodes[0].data = "Score: " + leftRightScore;
+        topBottomTricksInd.childNodes[0].data = "Tricks: " + topBottomTricks;
+        leftRightTricksInd.childNodes[0].data = "Tricks: " + leftRightTricks;
+        
+        // TODO: display current cards on screen and update trump indicators
+        
+        if(whoseTurn === yourPlayerNumber)
+        {
+            switch(phase)
+            {
+                case 0: // waiting for players to join the game
+                    // Set a 1.5-second AJAX request timer to refresh and see if anyone's joined
+                    window.setTimeout(function() { doAjaxRequest("", ""); }, 1500);
+                    // (this should never happen, since the turn is immediately passed on to the next guy when you join)
+                    break;
+                case 1: // choosing whether the dealer should pick up the turned-over card
+                    pickUpButtonsSpan.style.display = "inline";
+
+                    break;
+                case 2: // waiting for the dealer to choose a discard after taking the turned-over card
+                    respondToCardClick = true;
+
+                    break;
+                case 3: // picking the trump suit (if the dealer didn't pick up)
+                    suitSelectButtonsSpan.style.display = "inline";
+
+                    break;
+                case 4: // during the hand
+                    respondToCardClick = true;
+
+                    break;
+                case 5: // between tricks (waiting for players to ready up - this is automatically initiated by a JS timer on the client side)
+                    window.setTimeout(function() { doAjaxRequest("readyUp", ""); }, 1500);
+
+                    break;
+                case 6: // between hands (waiting for players to ready up - they need to click a button)
+                    readyButtonSpan.style.display = "inline";
+
+                    break;
+                case 7: // game over
+                    var resultInfoDiv = document.createElement("div");
+                    if(topBottomScore > leftRightScore)
+                        resultInfoDiv.appendChild(document.createTextNode("Game over - team " + yourTeamNum + " wins!"));
+                    else
+                    {
+                        var otherTeam;
+                        if(yourTeamNum === 1)
+                            otherTeam = 2;
+                        else
+                            otherTeam = 1;
+                        resultInfoDiv.appendChild(document.createTextNode("Game over - team " + otherTeam + " wins."));
+                    }
+                    infoNode.appendChild(resultInfoDiv);
+                    resultInfoDiv = document.createElement("div")
+                    var linkNode = document.createElement("a");
+                    linkNode.setAttribute("href", "index.jsp");
+                    linkNode.appendChild(document.createTextNode("Click here"));
+                    resultInfoDiv.appendChild(linkNode);
+                    resultInfoDiv.appendChild(document.createTextNode(" to return to the main menu."));
+                    infoNode.appendChild(resultInfoDiv);
+            }
+        }
+        else
+            window.setTimeout(function() { doAjaxRequest("", ""); }, 1500);
     }
 }
